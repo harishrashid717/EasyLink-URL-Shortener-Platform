3a60fc76c10bc4b8ce03240ddeeaee76472b135b
Harish Rashid <91613286+harishrashid717@users.noreply.github.com>
2025-07-10 17:10:26 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Auth Middleware (Optional)
+// Auth Middleware 
 app.use(authOptionalMiddleware);
 
 // Home Route
